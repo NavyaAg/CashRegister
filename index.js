148b5d01f93346fe77d1f5daccc2ce78a9631472
NavyaAg <itsNavya001@gmail.com>
2021-09-09 17:52:27 +0530

    functionality added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var billAmt=document.querySelector(".billamt")
 var amtGiven=document.querySelector(".amtgiven")
 var calculate=document.querySelector("button")
+const noteNumber=document.querySelectorAll(".no-of-notes")
+
+const denomination=[2000,500,100,20,10,5,1]
 calculate.addEventListener("click",function validateAmt(){
     if(billAmt.value>0 &&  amtGiven.value>=billAmt.value)
     {
-      calculateChange(billAmt.value,amtGiven.value)
+        var moneyLeft=amtGiven.value-billAmt.value;
+      calculateChange(moneyLeft)
     }
     else
     {
@@ -12,24 +16,16 @@ calculate.addEventListener("click",function validateAmt(){
     }
 })
 
-function calculateChange(bill,amt){
-    var change=amt-bill;
+function calculateChange(change){
+    
+    console.log(change);
+    for(i=0;i<=6;i++){
+        var noOfnotes=Math.trunc(change/denomination[i]);
+        change%=denomination[i];
+        noteNumber[i].innerText=noOfnotes;
+    }
     
 }
 
-/*change.addEventListener("click",function(){
-    if( inputBill.value<=inputAmt.value)
-    {changeCalcuator(inputBill.value,inputAmt.value)}
-    else if(inputBill.value<inputAmt.value)
-   { alert("fail")
-   console.log("clicked")
-}
-   
-
-})
-
-function changeCalcuator(bill,amt){
-    alert("success")
-    console.log(bill)
-    console.log(amt)
-}*/
+/*Doesnt accept all nos look into it
+Add styling*/
